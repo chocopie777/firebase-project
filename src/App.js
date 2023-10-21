1bcdeb61706a2d9826f45ecee99349a738982593
chocopie777 <egoran99@gmail.com>
2023-10-22 00:52:39 +0500

    created auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import * as firebaseDatabase from 'firebase/database'
+import * as firebaseAuth from 'firebase/auth'
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    componentDidMount() {
+        const db = firebaseDatabase.getDatabase();
+        console.log(db);
+    }
+
+    handleChange = ({target: {value, id}}) => {
+        this.setState({
+            [id]: value
+        })
+    }
+
+    createAccount = () => {
+        const {email, password} = this.state;
+        const auth = firebaseAuth.getAuth();
+        // firebaseAuth.createUserWithEmailAndPassword(auth, email, password)
+        //     .catch(error => console.log(error));
+        firebaseAuth.signInWithEmailAndPassword(auth, email, password)
+            .catch()
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                <div className="login_block">
+                    <input type="text" placeholder="email" id="email" onChange={this.handleChange}/>
+                    <input type="password" id="password" placeholder="password" onChange={this.handleChange}/>
+                    <input type="submit" onClick={this.createAccount}/>
+                </div>
+            </div>
+        )
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase/app';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDWm20LEUvctdAvyZ_u3WSWztArlplHFGM",
+    authDomain: "fir-test-project-9d867.firebaseapp.com",
+    databaseURL: "https://fir-test-project-9d867-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-test-project-9d867",
+    storageBucket: "fir-test-project-9d867.appspot.com",
+    messagingSenderId: "128691373686",
+    appId: "1:128691373686:web:309b57903588aaa28e8157"
+}
+firebase.initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
